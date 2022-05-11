106b243ab97a5ac9065331726ed1f9145113ea32
mahaamesha <mahaamesha@gmail.com>
2022-05-11 23:51:38 +0700

    fixed. check repo: fish-length-opencv

--- test/0507_02/0507_02.py
+++ test/0507_02/0507_02.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 
 #path = 'test/0507_02/img'
 path = 'img'
@@ -17,3 +17,12 @@ for i in list:
 	# cmd /k : remain after run
 	# cmd /c : terminate after run
 	os.system('cmd /c "python main.py -i img/%s.jpg"' %i)
+
+	#tmp_num = []
+	#tmp_length = []
+	#tmp_encod = []
+	print('tampung nilai num_fish & fishlength & encoded_string')	# ada 5 data untuk setiap list
+	
+	# Create tmp.json
+	# Access data from result.json in fish-length-opencv
+	# Append the dict into tmp.json
