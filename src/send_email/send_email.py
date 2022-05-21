5ac425e21dcec7766233a0e55a28d23ba480becd
mahaamesha <mahaamesha@gmail.com>
2022-05-22 00:21:54 +0700

    clear send_email.py

---
+++ src/send_email/send_email.py
@@ -0,0 +1,41 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+
+recever_email = '10219084@mahasiswa.itb.ac.id'
+sender_email = 'maraspi084@gmail.com'
+sender_pass = 'EsVd4A72_nhi7m!'
+subject = '[Report] IoT Aquaculture'
+
+# Build structure of email
+msg = MIMEMultipart()
+msg['Subject'] = subject
+msg['From'] = sender_email
+msg['To'] = recever_email
+
+# Attach body of email
+html_path = 'src/send_email/body.html'
+body = open(html_path, 'r').read()
+#body = 'asdadad'
+part_text = MIMEText(body, 'html')
+msg.attach(part_text)
+
+# Attach image to email
+img_path = 'src/fish-length-opencv/imgcv/final.jpg'
+part_img = MIMEBase('image', 'jpg')
+part_img.set_payload( open(img_path, 'rb').read() )
+encoders.encode_base64(part_img)
+part_img.add_header('Content-Disposition', 'attachment', filename='final.jpg')
+msg.attach(part_img)
+
+try:
+	with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+		smtp.ehlo()     #Identify ourselves with the mail server we are using. 
+		smtp.starttls() #Encrypt our connection
+		smtp.ehlo()     #Reidentify our connection as encrypted with the mail server
+		smtp.login(sender_email, sender_pass)
+		smtp.sendmail(sender_email, recever_email, msg.as_string())
+except:
+	print('Error: unable to send email')
--- test/0511_01/img_email.py
+++ test/0511_01/img_email.py
@@ -6,21 +6,21 @@ from email.mime.text import MIMEText
 from email.utils import formatdate  
 from email import encoders  
 
-img_path = 'test/0511_01/image.jpg'
 
 def send_an_email():  
-    toaddr = '10219084@mahasiswa.itb.ac.id'      # To id 
-    me = 'maraspi084@gmail.com'          # your id
-    pass_me = 'EsVd4A72_nhi7m!'
+    receiver_email = '10219084@mahasiswa.itb.ac.id'      # To id 
+    sender_email = 'maraspi084@gmail.com'          # your id
+    sender_pass = 'EsVd4A72_nhi7m!'
     subject = "dududu"              # Subject
   
     msg = MIMEMultipart()  
     msg['Subject'] = subject  
-    msg['From'] = me  
-    msg['To'] = toaddr  
+    msg['From'] = sender_email  
+    msg['To'] = receiver_email  
     msg.preamble = "test "   
     #msg.attach(MIMEText(text))  
   
+    img_path = 'test/0511_01/image.jpg'
     part = MIMEBase('application', "octet-stream")  
     part.set_payload(open(img_path, "rb").read())  
     encoders.encode_base64(part)  
@@ -28,14 +28,14 @@ def send_an_email():
     msg.attach(part)  
   
     try:  
-       s = smtplib.SMTP('smtp.gmail.com', 587)  # Protocol
-       s.ehlo()  
-       s.starttls()  
-       s.ehlo()  
-       s.login(user = me, password = pass_me)  # User id & password
+       server = smtplib.SMTP('smtp.gmail.com', 587)  # Protocol
+       server.ehlo()  
+       server.starttls()  
+       server.ehlo()  
+       server.login(user = sender_email, password = sender_pass)  # User id & password
        #s.send_message(msg)  
-       s.sendmail(me, toaddr, msg.as_string())  
-       s.quit()  
+       server.sendmail(sender_email, receiver_email, msg.as_string())  
+       server.quit()  
     #except:  
     #   print ("Error: unable to send email")    
     except SMTPException as error:  
