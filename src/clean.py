259e4b51a68fecdcefe3489e5da197268dd8f156
mahaamesha <mahaamesha@gmail.com>
2022-05-28 22:47:10 +0700

    use os.system()

--- src/clean.py
+++ src/clean.py
@@ -12,19 +12,13 @@ def clean(list_dir=target):
 	working_path = os.path.dirname(__file__)    # in src/
 	project_path = os.path.join(working_path, '../')    # in iot-aquaculture/
 
-	for dir in target:
+	for dir in list_dir:
 		dir_path = os.path.join(project_path, dir)
 
-		list_file = os.listdir(dir_path)
-		print(list_file)
-
-		# Delete all files in dir_path
-		for file_name in list_file:
-			file_path = os.path.join(dir_path, file_name)
-			try:
-				os.remove(file_path)
-			except: 
-				print('Not found:', file_path)
+		try:
+			os.system('sudo rm -rf %s' %dir_path)
+		except:
+			print('Error: run this program in linux')
 
 if __name__ == '__main__':
 	clean(list_dir=target)
