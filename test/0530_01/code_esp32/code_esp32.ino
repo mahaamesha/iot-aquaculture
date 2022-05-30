80b9906b5fcc87421c50d181e19079786f220e4f
mahaamesha <mahaamesha@gmail.com>
2022-05-31 01:46:31 +0700

    define pin

--- test/0530_01/code_esp32/code_esp32.ino
+++ test/0530_01/code_esp32/code_esp32.ino
@@ -18,22 +18,24 @@
   - Select your ESP8266 in "Tools -> Board"
 */
 
-#include <WiFi.h>
+#include <WiFi.h>	// ESP32
+//#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
 // ds18b20
 #include <OneWire.h>
 #include <DallasTemperature.h>
-OneWire wiring(6);
-DallasTemperature sensor(&wiring);
+int TEMP_PIN = 4;	// pin D4 - GPIO4 - ADC2_0
+OneWire wiring(TEMP_PIN);
+DallasTemperature ds18b20(&wiring);
 
 // pH4502c
-int pHSense = A0;
+int PH_PIN = 34;	// pin D34 - GPIO34 - ADC1_6
 int samples = 10;
 float adc_resolution = 1024.0;
 
 // turbidity sensor
-int sensorPin = A0;
+int TURB_PIN = 35;	// pin D35 - GPIO35 - ADC1_7
 
 // Update these with values suitable for your network.
 const char* ssid = "Wi-Fe";
@@ -45,14 +47,13 @@ PubSubClient client(espClient);
 unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE	(50)
 char msg[MSG_BUFFER_SIZE];
-int value = 0;
 
 
 // Function temperature
 float read_temperature() {
-  sensor.setResolution(9);
-  sensor.requestTemperatures();
-  float dataSuhu = sensor.getTempCByIndex(0);
+  ds18b20.setResolution(9);
+  ds18b20.requestTemperatures();
+  float dataSuhu = ds18b20.getTempCByIndex(0);
   
   return dataSuhu;
 }
@@ -64,7 +65,7 @@ float read_pH() {
   int measuring = 0;
 
   for(int i=0;i<samples;i++){
-    measuring += analogRead(pHSense);	// pin pHSense in void setup()
+    measuring += analogRead(PH_PIN);	// pin PH_PIN in void setup()
     delay(10);
   }
 
@@ -88,7 +89,7 @@ float read_turbidity() {
   float ntu = 0;
   
   for(int i=0; i<800; i++) {
-    volt += ((float)analogRead(sensorPin)/1023)*5;
+    volt += ((float)analogRead(TURB_PIN)/1023)*5;
   }
   volt = volt/800;
   volt = round_to_dp(volt,2);
@@ -181,7 +182,7 @@ void setup() {
   client.setCallback(callback);
 
   // ph4502c
-  pinMode(pHSense, INPUT);
+  pinMode(PH_PIN, INPUT);
 }
 
 void loop() {  
@@ -194,15 +195,17 @@ void loop() {
   unsigned long now = millis();
   if (now - lastMsg > 2000) {
     lastMsg = now;
-    ++value;
-    snprintf (msg, MSG_BUFFER_SIZE, "Avima 2 berhasil #%ld", value);
     Serial.print("Publish message: ");
     Serial.println(msg);
 
-	float temperature = read_temperature();
-	float pH = read_pH();
-	float turbidity = read_turbidity();
-	
+	//float temperature = read_temperature();
+	//float pH = read_pH();
+	//float turbidity = read_turbidity();
+
+	float temperature = 24;
+	float pH = 6;
+	float turbidity = 45;
+
 	String message = String(temperature) + "," + String(pH) + "," + String(turbidity);
     client.publish("ourSensorOut", message.c_str());
   }
